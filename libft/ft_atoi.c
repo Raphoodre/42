7143910c0c9cd3c338c05626d367cdbf488e8218
Raphaël Ivol <raphael.ivolpro@gmail.com>
2023-04-14 18:18:11 +0200

    atoi

---
+++ libft/ft_atoi.c
@@ -0,0 +1,46 @@
+#include "libft.h"
+
+int ft_atoi(const char *str) {
+    int result = 0;
+    int sign = 1;
+    int i = 0;
+
+    // Ignore les espaces au début de la chaîne
+    while (str[i] == ' ') {
+        i++;
+    }
+
+    // Gère le signe + ou -
+    if (str[i] == '-') {
+        sign = -1;
+        i++;
+    } else if (str[i] == '+') {
+        i++;
+    }
+
+    // Convertit chaque caractère de la chaîne en un entier
+    while (str[i] >= '0' && str[i] <= '9') {
+        result = result * 10 + (str[i] - '0');
+        //En effet, lorsqu'on ajoute un nouveau chiffre à la partie entière, 
+        //on veut l'ajouter à la position des unités, puis des dizaines, des centaines, etc. 
+        //Pour cela, on doit multiplier le résultat précédent par 10 pour décaler les chiffres vers la gauche et créer une nouvelle position à droite pour le nouveau chiffre. 
+        //Par exemple, si le résultat précédent est 123 et que le nouveau chiffre est 4, on doit calculer 123 * 10 + 4 = 1234 pour obtenir le nouveau résultat.
+        i++;
+    }
+
+    // Applique le signe et renvoie le résultat
+    return sign * result;
+}
+
+int main()
+{
+    int native;
+    int mine;
+
+    native = atoi("   -3455e2");
+    mine = ft_atoi("   -3455e2");
+    printf("Atoi : %d", native);
+    printf("\n");
+    printf("My Atoi : %d", mine);
+    return 0;
+}

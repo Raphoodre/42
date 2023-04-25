6acd5756a0456d5509a60f9a6e6b8602d6c77637
Raphaël Ivol <raphael.ivolpro@gmail.com>
2023-04-25 18:53:19 +0200

    Added memcpy & memset

---
+++ libft/ft_memset.c
@@ -0,0 +1,18 @@
+#include "libft.h"
+
+void *ft_memset(void *s, int c, size_t n)
+{
+    size_t i;
+
+    if (!s)
+        return (NULL);
+
+    i = 0;
+
+    while ( i < n)
+    {
+        *((unsigned char*) s + i) = (unsigned char)c;
+        i++;
+    }
+    return (s);
+}
--- libft/libft.h
+++ libft/libft.h
@@ -20,6 +20,7 @@ int ft_tolower(int c);
 int ft_toupper(int c);
 void *ft_memchr(const void *s, int c, size_t n);
 int ft_memcmp(const void *s1, const void *s2, size_t n);
-
+void *ft_memset(void *s, int c, size_t n);
+void *ft_memcpy(void *dest, const void *src, size_t n);
 
 #endif
